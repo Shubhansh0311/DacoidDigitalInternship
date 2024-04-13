68f4e28e7433099d3fc4692643fea61560fe9d9a
Shubhansh0311 <shubhanshagrawal0311@gmail.com>
2024-04-13 14:45:29 +0530

    project completed

--- src/Signup.js
+++ src/Signup.js
@@ -6,7 +6,7 @@ import { IoWifi } from "react-icons/io5";
 import { IoBatteryFull } from "react-icons/io5";
 const Signup = () => {
   return (
-    <div className="max-w-[480px] border-1 rounded-2xl pb-0[88px] shadow-lg shadow-gray-300 border-gray-400 h-[100%] flex w-full  py-1 px-2 mx-auto  justify-center">
+    <div className="max-w-[480px] border-1 rounded-2xl pb-[88px] shadow-lg shadow-gray-300 border-gray-400 h-[100%] flex w-full  py-1 px-2 mx-auto  justify-center">
       <div>
         <div className="status bar flex justify-between">
           <span>9:41</span>
